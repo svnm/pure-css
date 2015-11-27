19ccb964372818dde8d6d803d4450685e8dbc238
Steven Iseki Martin <stevenisekimartin@gmail.com>
2015-11-27 13:49:43 +1100

    update to pure-css example

--- example/src/reducers/list.js
+++ example/src/reducers/list.js
@@ -1,23 +1,4 @@
-const initialState = {
-  friendsAdded: 3,
-  friends: [ 
-    {
-      id: 1,
-      name: 'Brian Lara',
-      starred: false
-    },
-    {
-      id: 2,
-      name: 'Sachin Tendulkar',
-      starred: false
-    },
-    {
-      id: 3,
-      name: 'Ricky Ponting',
-      starred: false
-    }
-  ]
-}
+const initialState = {}
 
 export default function list(state = initialState, action) {
   switch (action.type) {
