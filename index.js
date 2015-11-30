b624836b6463d5ed1059df88ab1078459554ebad
Steven Iseki Martin <stevenisekimartin@gmail.com>
2015-11-30 16:17:42 +1100

    update to index.js

--- index.js
+++ index.js
@@ -1,46 +1,21 @@
 'use strict';
 
-exports.__esModule = true;
+var base = require('./lib/base.css');
+var buttons = require('./lib/buttons.css');
+var formsNr = require('./lib/forms-nr.css');
+var forms = require('./lib/forms.css');
+var gridsNr = require('./lib/grids-nr.css');
+var grids = require('./lib/grids.css');
+var menus = require('./lib/menus.css');
+var tables = require('./lib/tables.css');
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-var _libBaseCss = require('./lib/base.css');
-
-var _libBaseCss2 = _interopRequireDefault(_libBaseCss);
-
-var _libButtonsCss = require('./lib/buttons.css');
-
-var _libButtonsCss2 = _interopRequireDefault(_libButtonsCss);
-
-var _libFormsNrCss = require('./lib/forms-nr.css');
-
-var _libFormsNrCss2 = _interopRequireDefault(_libFormsNrCss);
-
-var _libFormsCss = require('./lib/forms.css');
-
-var _libFormsCss2 = _interopRequireDefault(_libFormsCss);
-
-var _libGridsNrCss = require('./lib/grids-nr.css');
-
-var _libGridsNrCss2 = _interopRequireDefault(_libGridsNrCss);
-
-var _libGridsCss = require('./lib/grids.css');
-
-var _libGridsCss2 = _interopRequireDefault(_libGridsCss);
-
-var _libMenusCss = require('./lib/menus.css');
-
-var _libMenusCss2 = _interopRequireDefault(_libMenusCss);
-
-var _libTablesCss = require('./lib/tables.css');
-
-var _libTablesCss2 = _interopRequireDefault(_libTablesCss);
-
-exports.base = _libBaseCss2['default'];
-exports.buttons = _libButtonsCss2['default'];
-exports.formsNr = _libFormsNrCss2['default'];
-exports.forms = _libFormsCss2['default'];
-exports.gridsNr = _libGridsNrCss2['default'];
-exports.grids = _libGridsCss2['default'];
-exports.menus = _libMenusCss2['default'];
-exports.tables = _libTablesCss2['default'];
+module.exports = {
+	base: base,
+	buttons: buttons,
+	formsNr: formsNr,
+	forms: forms,
+	gridsNr: gridsNr,
+	grids: grids,
+	menus: menus,
+	tables: tables
+};
